91201ca344df70c846cc32e9fd038ace2d184ff8
tasmankingsley <tasman.kingsley@gmail.com>
2022-03-13 22:17:31 +1100

    tweaks

--- src/App.svelte
+++ src/App.svelte
@@ -1,52 +1,58 @@
 <script>
   let days = [];
 
-  for (let index = 0; index < 31; index++) {
-    days[index] = index;
+  for (let index = 0; index < 49; index++) {
+    days[index] = index + 1;
   }
   console.log(days);
 </script>
 
 <div class="main">
-    <div class="grid">
-      {#each days as day}
-        <div class="day">{day}</div>
-      {/each}
-    </div>
+  <span contenteditable="true">pushup days</span>
+  <div class="grid">
+    {#each days as day}
+      <div class="day"></div>
+    {/each}
+  </div>
 </div>
 
 <style>
 .main {
-  display: flex;
-  flex-direction: column;
-  justify-items: center;
-  height: 100vh;
+  display: grid;
+  grid-auto-flow: row;
+  grid-template-rows: 80px 1fr;
+  height: 100%;
+}
+
+span {
+  text-align: center;
+  margin: auto;
 }
 
 .grid {
   display: grid;
   grid-auto-flow: column;
-  grid-template-rows: repeat(6, 1fr);
-  grid-template-columns: repeat(6, 1fr);;
+  grid-template-rows: repeat(7, 1fr);
+  grid-template-columns: repeat(7, 1fr);;
   width: auto;
   height: auto;
   margin: auto;
   background-color: #1e1f29;
   border-radius: 12px;
-  row-gap: 10px;
-  column-gap: 10px;
-  padding: 10px;
+  row-gap: 6px;
+  column-gap: 6px;
+  padding: 12px;
   /* border: 1px solid white; */
 }
 
 .day {
-  width: 38px;
-  height: 38px;
-  line-height: 38px;
+  width: 25px;
+  height: 25px;
+  line-height: 25px;
   font-size: 1.5rem;
   background-color: #00fa9a;
-  padding: 5px;
-  border-radius: 7px;
+  /* padding: 5px; */
+  border-radius: 5px;
   text-align: center;
 }
 
