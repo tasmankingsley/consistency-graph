210286fef5cad1bbb21ca3b267d9d1447a5281da
tasmankingsley <tasman.kingsley@gmail.com>
2022-03-13 20:39:06 +1100

    grid fix

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,7 @@
 <div class="main">
     <div class="grid">
       {#each days as day}
-        <div class="day"></div>
+        <div class="day">{day}</div>
       {/each}
     </div>
 </div>
@@ -26,24 +26,27 @@
 .grid {
   display: grid;
   grid-auto-flow: column;
-  grid-template-rows: repeat(5, 1fr);
+  grid-template-rows: repeat(6, 1fr);
   grid-template-columns: repeat(6, 1fr);;
-  width: 280px;
-  height: 280px;
+  width: auto;
+  height: auto;
   margin: auto;
   background-color: #1e1f29;
-  border-radius: 10px;
-  row-gap: 5px;
-  column-gap: 5px;
-  padding: 5px;
+  border-radius: 12px;
+  row-gap: 10px;
+  column-gap: 10px;
+  padding: 10px;
+  /* border: 1px solid white; */
 }
 
 .day {
-  width: 1.5rem;
-  height: 1.5rem;
+  width: 38px;
+  height: 38px;
+  line-height: 38px;
+  font-size: 1.5rem;
   background-color: #00fa9a;
   padding: 5px;
-  border-radius: 4px;
+  border-radius: 7px;
   text-align: center;
 }
 
